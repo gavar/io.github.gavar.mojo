5946ccd11f0d1bf108f9d81e3c90191359c4575f
gavar <max.stankevich.me@gmail.com>
2019-12-04 03:06:02 +0200

    feat: common validation tools

---
+++ mojo-commons/src/main/java/dev/gavar/mojo/validate/Preconditions.java
@@ -0,0 +1,102 @@
+package dev.gavar.mojo.validate;
+
+/**
+ * Assertion utility providing set of methods for validating program input.
+ * These methods generally accept a{@code boolean} expression which is expected to be {@code true}.
+ */
+public class Preconditions {
+
+    private static ThreadLocal<StringBuilder> threadLocalStringBuilder = ThreadLocal.withInitial(StringBuilder::new);
+
+    /**
+     * Ensures the truth of an expression.
+     * @param expression - expression to check.
+     * @throws IllegalArgumentException when expression evaluates to {@literal false}.
+     */
+    public static void checkArgument(boolean expression) {
+        if (expression) return;
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     * @param expression - expression to check.
+     * @param error      - exception message to use if the check fails.
+     * @throws IllegalArgumentException when expression evaluates to {@literal false}.
+     */
+    public static void checkArgument(boolean expression, Object error) {
+        if (expression) return;
+        String message = error != null ? error.toString() : null;
+        throw new IllegalArgumentException(message);
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     * @param expression - expression to check.
+     * @param format     - exception message format string.
+     * @throws IllegalArgumentException when expression evaluates to {@literal false}.
+     */
+    public static void checkArgument(boolean expression, String format, Object $1) {
+        if (expression) return;
+        String message = format(format, $1);
+        throw new IllegalArgumentException(message);
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     * @param expression - expression to check.
+     * @param format     - exception message format string.
+     * @throws IllegalArgumentException when expression evaluates to {@literal false}.
+     */
+    public static void checkArgument(boolean expression, String format, Object $1, Object $2) {
+        if (expression) return;
+        String message = format(format, $1, $2);
+        throw new IllegalArgumentException(message);
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     * @param expression - expression to check.
+     * @param format     - exception message format string.
+     * @throws IllegalArgumentException when expression evaluates to {@literal false}.
+     */
+    public static void checkArgument(boolean expression, String format, Object $1, Object $2, Object $3) {
+        if (expression) return;
+        String message = format(format, $1, $2, $3);
+        throw new IllegalArgumentException(message);
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     * @param expression - expression to check.
+     * @param format     - exception message format string.
+     * @param args       - arguments to use for substituting literals in format message.
+     * @throws IllegalArgumentException when expression evaluates to {@literal false}.
+     */
+    public static void checkArgument(boolean expression, String format, Object... args) {
+        if (expression) return;
+        String message = format(format, args);
+        throw new IllegalArgumentException(message);
+    }
+
+    public static String format(String message, Object... args) {
+        if (message == null) return null;
+        if (args == null || args.length < 1) return message;
+        StringBuilder sb = threadLocalStringBuilder.get();
+        try {
+            sb.ensureCapacity(message.length() + 1);
+            int offset = 0;
+            for (int i = 0; i < args.length; i++) {
+                int index = message.indexOf("%s", offset);
+                if (index == -1) break;
+                sb.append(message, offset, index);
+                sb.append(args[i++]);
+                offset = index + 2;
+            }
+            sb.append(message, offset, message.length());
+            return sb.toString();
+        } finally {
+            sb.setLength(0);
+        }
+    }
+}
---
+++ mojo-commons/src/main/java/dev/gavar/mojo/validate/UnableTo.java
@@ -0,0 +1,60 @@
+package dev.gavar.mojo.validate;
+
+import java.util.Collection;
+
+import static dev.gavar.mojo.validate.Preconditions.checkArgument;
+
+/**
+ * Provides set of validation methods same as {@link Validate},
+ * but throwing error in form of describing some action like "unable to .... because ....".
+ */
+public class UnableTo {
+
+    /**
+     * Throw an exception when provided value is null.
+     * @param action name of the action which unable to perform on check failure.
+     * @param name   name of the argument.
+     * @param value  argument value to check.
+     * @throws IllegalArgumentException when check fails.
+     */
+    public static <T> void whenArgumentIsNull(String action, String name, T value) {
+        final boolean NotNull = value != null;
+        checkArgument(NotNull, "unable to '%s' because argument '%s' is null", action, name);
+    }
+
+    /**
+     * Throw an exception when provided array is null or empty.
+     * @param action name of the action which unable to perform on check failure.
+     * @param name   name of the argument.
+     * @param array  argument value to check.
+     * @throws IllegalArgumentException when check fails.
+     */
+    public static <T> void whenArgumentIsEmpty(String action, String name, T[] array) {
+        final boolean NotEmpty = array != null && array.length > 0;
+        checkArgument(NotEmpty, "unable to '%s' because argument '%s' is empty", action, name);
+    }
+
+    /**
+     * Throw an exception when provided char sequence is null or empty.
+     * @param action name of the action which unable to perform on check failure.
+     * @param name   name of the argument.
+     * @param chars  argument value to check.
+     * @throws IllegalArgumentException when check fails.
+     */
+    public static void whenArgumentIsEmpty(String action, String name, CharSequence chars) {
+        final boolean NotEmpty = chars != null && chars.length() > 0;
+        checkArgument(NotEmpty, "unable to '%s' because argument '%s' is empty", action, name);
+    }
+
+    /**
+     * Throw an exception when provided collection is null or empty.
+     * @param action     name of the action which unable to perform on check failure.
+     * @param name       name of the argument.
+     * @param collection argument value to check.
+     * @throws IllegalArgumentException when check fails.
+     */
+    public static void whenArgumentIsEmpty(String action, String name, Collection collection) {
+        final boolean NotEmpty = collection != null && !collection.isEmpty();
+        checkArgument(NotEmpty, "unable to '%s' because argument '%s' is empty", action, name);
+    }
+}
