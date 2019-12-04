05471ca06fa7ce69a9291497d78cc329340661ca
gavar <max.stankevich.me@gmail.com>
2019-12-04 03:25:05 +0200

    feat: jooq-liquibase-maven-plugin

---
+++ jooq-liquibase-maven-plugin/src/main/java/dev/gavar/mojo/rc/AbstractChangeLogMojo.java
@@ -0,0 +1,59 @@
+package dev.gavar.mojo.rc;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.FileSystemResourceAccessor;
+import liquibase.resource.ResourceAccessor;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.sql.Connection;
+
+public abstract class AbstractChangeLogMojo extends AbstractTestContainerMojo {
+
+    /** Specifies the change log file to use for Liquibase. */
+    @Parameter(property = "liquibase.changeLogFile")
+    protected String changeLogFile;
+
+    /**
+     * The Liquibase contexts to execute, which can be "," separated if multiple contexts
+     * are required. If no context is specified then ALL contexts will be executed.
+     */
+    @Parameter(property = "liquibase.contexts", defaultValue = "")
+    protected String contexts;
+
+    /**
+     * The Liquibase labels to execute, which can be "," separated if multiple labels
+     * are required or a more complex expression. If no label is specified then ALL all will be executed.
+     */
+    @Parameter(property = "liquibase.labels", defaultValue = "")
+    protected String labels;
+
+    protected Liquibase createLiquibase(Connection connection) throws MojoExecutionException {
+        try {
+            final JdbcConnection jdbcConnection = new JdbcConnection(connection);
+            final DatabaseFactory databaseFactory = DatabaseFactory.getInstance();
+            final Database database = databaseFactory.findCorrectDatabaseImplementation(jdbcConnection);
+            final ResourceAccessor resourceAccessor = new FileSystemResourceAccessor();
+            return new Liquibase(changeLogFile, resourceAccessor, database);
+        } catch (DatabaseException e) {
+            throw new MojoExecutionException("liquibase initialization error", e);
+        }
+    }
+
+    protected void liquibaseUpdate(Liquibase liquibase) throws MojoExecutionException {
+        final Contexts contexts = new Contexts(this.contexts);
+        final LabelExpression labelExpression = new LabelExpression(labels);
+        try {
+            liquibase.update(contexts, labelExpression);
+        } catch (LiquibaseException cause) {
+            throw new MojoExecutionException("unable to run liquibase update", cause);
+        }
+    }
+}
---
+++ jooq-liquibase-maven-plugin/src/main/java/dev/gavar/mojo/rc/AbstractTestContainerMojo.java
@@ -0,0 +1,71 @@
+package dev.gavar.mojo.rc;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+
+import java.lang.reflect.Constructor;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static dev.gavar.mojo.validate.Preconditions.checkArgument;
+import static dev.gavar.mojo.validate.Validate.argumentNotBlank;
+import static dev.gavar.mojo.validate.Validate.argumentOfType;
+
+public abstract class AbstractTestContainerMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    protected MavenProject project;
+
+    /** Name of the class implementing {@link JdbcDatabaseContainer}. */
+    @Parameter(property = "jdbc.container", required = true)
+    protected String jdbcContainer;
+
+    /** Name of the docker image containing JDBC container to run. */
+    @Parameter(property = "jdbc.docker-image-name", required = true)
+    protected String dockerImageName;
+
+    protected JdbcDatabaseContainer createJdbcContainer() throws MojoExecutionException {
+        argumentNotBlank(jdbcContainer, "jdbcContainer");
+        argumentNotBlank(dockerImageName, "dockerImageName");
+        try {
+            // resolve constructor
+            final ClassLoader classLoader = this.getClass().getClassLoader();
+            final Class<?> jdbcContainerClass = classLoader.loadClass(this.jdbcContainer);
+            final Constructor<?> constructor = jdbcContainerClass.getConstructor();
+
+            // instantiate
+            final Object instance = constructor.newInstance();
+            argumentOfType(instance, JdbcDatabaseContainer.class, "jdbcContainer");
+            final JdbcDatabaseContainer container = (JdbcDatabaseContainer) instance;
+
+            // configure
+            container.setDockerImageName(dockerImageName);
+            return container;
+        } catch (ReflectiveOperationException cause) {
+            getLog().error(cause);
+            throw new MojoExecutionException("unable to instantiate JDBC container", cause);
+        }
+    }
+
+    protected Connection connect(final JdbcDatabaseContainer container) throws MojoExecutionException {
+        try {
+            checkArgument(container.isRunning(), "JDBC container is not running");
+            return container.createConnection("");
+        } catch (SQLException cause) {
+            getLog().error(cause);
+            throw new MojoExecutionException("unable to create JDBC connection", cause);
+        }
+    }
+
+    protected void close(final Connection connection) throws MojoExecutionException {
+        try {
+            if (connection != null)
+                connection.close();
+        } catch (SQLException cause) {
+            throw new MojoExecutionException("unable to close JDBC connection", cause);
+        }
+    }
+}
